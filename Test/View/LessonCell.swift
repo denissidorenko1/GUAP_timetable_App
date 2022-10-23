0a3a33a0ad393d39454d82ad5cfcdd9b7dd97951
Denis <fpv.airplane.barnaul@gmail.com>
2022-10-23 23:30:07 +0300

    переписал лейблы внутри ячейки таблицы на констрейнты

--- Test/View/LessonCell.swift
+++ Test/View/LessonCell.swift
@@ -30,6 +30,16 @@ class LessonCell: UITableViewCell{
         lessonType.textColor = .orange
         teacher.font = teacher.font.withSize(14)
         groups.font = groups.font.withSize(14)
+        
+        
+        startTime.translatesAutoresizingMaskIntoConstraints = false
+        endTime.translatesAutoresizingMaskIntoConstraints = false
+        lessonNumber.translatesAutoresizingMaskIntoConstraints = false
+        lessonType.translatesAutoresizingMaskIntoConstraints = false
+        subjectName.translatesAutoresizingMaskIntoConstraints = false
+        room.translatesAutoresizingMaskIntoConstraints = false
+        teacher.translatesAutoresizingMaskIntoConstraints = false
+        groups.translatesAutoresizingMaskIntoConstraints = false
     }
     
     
@@ -76,6 +86,71 @@ class LessonCell: UITableViewCell{
         
     }
     
+    private func setConstraints(){
+        let startTimeConstraints = [
+            startTime.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            startTime.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            startTime.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ]
+        
+        let endTimeConstraints = [
+            endTime.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            endTime.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            endTime.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5)
+        ]
+        
+        let lessonNumberConstraints = [
+            lessonNumber.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            lessonNumber.topAnchor.constraint(equalTo: startTime.bottomAnchor, constant: 0),
+            lessonNumber.bottomAnchor.constraint(equalTo: endTime.topAnchor, constant: 0),
+        ]
+        
+        let lessonTypeConstraints = [
+            lessonType.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 2),
+            lessonType.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 75),
+            lessonType.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
+        ]
+        
+        let subjectNameConstraints = [
+            subjectName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 75),
+            subjectName.topAnchor.constraint(equalTo: lessonType.bottomAnchor, constant: 0),
+            subjectName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
+        ]
+        
+        let roomConstraints = [
+            room.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 75),
+            room.topAnchor.constraint(equalTo: subjectName.bottomAnchor, constant: 0),
+        ]
+        let groupConstraints = [
+            groups.leadingAnchor.constraint(equalTo: room.trailingAnchor, constant: 10),
+            groups.topAnchor.constraint(equalTo: subjectName.bottomAnchor, constant: 0),
+            // уезжает за ячейку группа, если пара на кафедре вне расписания
+            groups.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
+        ]
+        groupConstraints[0].priority = UILayoutPriority(700)
+        groupConstraints[2].priority = UILayoutPriority(2)
+        
+        
+        let teacherConstraints = [
+            teacher.topAnchor.constraint(equalTo: groups.bottomAnchor),
+            teacher.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 75),
+            teacher.topAnchor.constraint(equalTo: room.bottomAnchor, constant: 0),
+            teacher.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+            teacher.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ]
+        
+        
+        NSLayoutConstraint.activate(startTimeConstraints)
+        NSLayoutConstraint.activate(endTimeConstraints)
+        NSLayoutConstraint.activate(lessonNumberConstraints)
+        NSLayoutConstraint.activate(lessonTypeConstraints)
+        NSLayoutConstraint.activate(subjectNameConstraints)
+        NSLayoutConstraint.activate(roomConstraints)
+        NSLayoutConstraint.activate(groupConstraints)
+        NSLayoutConstraint.activate(teacherConstraints)
+    }
+    
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -89,7 +164,8 @@ class LessonCell: UITableViewCell{
         contentView.layer.shadowOpacity = 0.35
         contentView.layer.shadowOffset = CGSize(width: 0, height: 0)
         setFontsOfLabels()
-        setFramesOfLabels()
+//        setFramesOfLabels()
+        setConstraints()
         self.backgroundColor = .clear
     }
     
